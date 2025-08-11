6fcba199aaa42757e25ae6a47282ed89129fef71
Matiascalazan <matiascalazan@gmail.com>
2025-08-11 00:57:48 -0300

    primera pagina con react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola, mi nombra es Matias Calazan y esta es mi primer pagina con Reeact</h1>
+      <p>Materia: Desarrollo Web</p>
+    </div>
+  );
+}
+
+export default App;
