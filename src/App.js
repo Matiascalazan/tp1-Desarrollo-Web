03e5ad40587ba352776c29b746e71a9ab741f5d1
Matiascalazan <matiascalazan@gmail.com>
2025-09-06 22:07:56 -0300

    cambios tp2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Titulo from './Titulo';
+import TarjetaDePerfil from './TarjetaDePerfil';
+import ListaDeHabilidades from './ListaDeHabilidades';
 
 function App() {
+  const usuario = {
+    nombre: "Matías Calazan",
+    edad: 27,
+    profesion: "Técnico Electromecanico / Estudiante de Análisis de Sistemas",
+    ubicacion: "Aguas del Norte - Cafayate"
+  };
+
+  const habilidades = [
+    "Electricidad y tableros, Mecanica, Soldadura",
+    "Capacidad de adaptarme a cualquier entorno laboral",
+    "Aprendiendo Git y GitHub",
+    "Aprendiendo distintos lenguajes de programación",
+    "Análisis crítico y resolución de problemas"
+  ];
+
   return (
-    <div className="App">
-      <h1>Hola, mi nombra es Matias Calazan y esta es mi primer pagina con Reeact</h1>
-      <p>Materia: Desarrollo Web</p>
+    <div className="App" style={{ maxWidth: 800, margin: "40px auto", padding: "0 16px" }}>
+      <Titulo
+        texto="Mi portfolio simple en React"
+        subtitulo="TP - Desarrollo Web"
+      />
+
+      <TarjetaDePerfil
+        nombre={usuario.nombre}
+        edad={usuario.edad}
+        profesion={usuario.profesion}
+        ubicacion={usuario.ubicacion}
+      />
+
+      <ListaDeHabilidades habilidades={habilidades} />
     </div>
   );
 }
